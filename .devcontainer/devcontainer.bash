0872a4b5802ca94432919b60221170af7e52f91d
vityaman <vityaman.dev@yandex.ru>
2025-02-28 09:50:49 +0300

    #4 Setup Typst
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- .devcontainer/devcontainer.bash
+++ .devcontainer/devcontainer.bash
@@ -3,8 +3,22 @@
 set -e
 
 PKGS=(
+    wget
+    fontconfig
     typst
 )
+
+install_font() {
+    FONT_DST=/usr/local/share/fonts
+
+    FONT_FILE="$1"
+    FONT_URL="$2"
+
+    mkdir -p "$FONT_DST"
+    wget -q -O "$FONT_DST/$FONT_FILE" "$FONT_URL"
+
+    fc-cache -fv
+}
 
 mkdir -p /devcontainer
 cd /devcontainer
@@ -17,9 +31,13 @@ if [ ! -d /devcontainer/yay ]; then
     chown -R builder:builder /devcontainer
     echo 'builder ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
 
-    pacman --noconfirm -S --needed base-devel git
+    pacman --noconfirm -S base-devel git
     su - builder -c "git clone https://aur.archlinux.org/yay.git /devcontainer/yay"
     su - builder -c "cd /devcontainer/yay && makepkg -si --noconfirm"
 fi
 
-yay --noconfirm -Sy $PKGS
+for pkg in "${PKGS[@]}"; do
+  yay --noconfirm -Sy $pkg
+done
+
+install_font "Times New Roman.ttf" https://github.com/justrajdeep/fonts/raw/refs/heads/master/Times%20New%20Roman.ttf
---
+++ source/main.typ
@@ -0,0 +1,22 @@
+#set page(
+  paper: "a4",
+  margin: (top: 20mm, bottom: 20mm, left: 30mm, right: 15mm),
+)
+
+#set text(
+  font: "Times New Roman",
+  size: 14pt,
+  spacing: 150%,
+)
+
+#set par(
+  justify: true,
+  spacing: 1.25cm,
+)
+
+#set page(numbering: "1")
+#counter(page).update(1)
+
+= Введение 
+
+В этом отчете мы рассмотрим различные факторы, влияющие на динамику жидкости в ледниках, и то, как они влияют на формирование и поведение этих природных структур.
